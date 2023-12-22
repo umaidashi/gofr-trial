2a0bc583e06dae7c85c6e0dc7a5b5e63e75b8198
umaidashi <umaidashi18@gmail.com>
2023-12-22 14:48:09 +0900

    feat: update

--- main.go
+++ main.go
@@ -1,31 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "gofr.dev/pkg/gofr"
+	"fmt"
+
+	"gofr.dev/pkg/gofr"
 )
 
+type user struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
-    app := gofr.New()
+	app := gofr.New()
 
-    app.GET("/", func(ctx *gofr.Context) (interface{}, error) {
-        return "index", nil
-    })
+	app.GET("/", func(ctx *gofr.Context) (interface{}, error) {
+		return "index", nil
+	})
 
-    // get all query param,
-    app.GET("/all", func(ctx *gofr.Context) (interface{}, error) {
-        return ctx.Params(), nil
-    })
+	app.GET("/user", func(ctx *gofr.Context) (interface{}, error) {
+		// get all query param,
+		// return ctx.Params(), nil
 
-    // get query param
-    app.GET("/user", func(ctx *gofr.Context) (interface{}, error) {
-        return fmt.Sprintf("Hello %s", ctx.Param("name")), nil
-    })
+		// get query param
+		return ctx.Param("name"), nil
+	})
 
-    // get path param
-    app.GET("/user/{id}", func(ctx *gofr.Context) (interface{}, error) {
-        return fmt.Sprintf("Hello %s", ctx.PathParam("id")), nil
-    })
+	// get path param
+	app.GET("/user/{id}", func(ctx *gofr.Context) (interface{}, error) {
+		return fmt.Sprintf("Hello %s", ctx.PathParam("id")), nil
+	})
 
-    app.Start()
+	// get body
+	app.POST("/user/new", func(ctx *gofr.Context) (interface{}, error) {
+		var u user
+		if err := ctx.BindStrict(&u); err != nil {
+			return nil, err
+		}
+		return u.Name, nil
+	})
+
+	// get Header
+	app.POST("/header", func(ctx *gofr.Context) (interface{}, error) {
+		return ctx.Header("Content-Type"), nil
+	})
+
+	app.Start()
 }
